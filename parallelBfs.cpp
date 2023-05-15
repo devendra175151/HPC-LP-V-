0a03ece57aa4326b782893f6f46a774de72b5776
Devendra Solunke <SOLUNKEDEVENDRA@GMAIL.COM>
2023-05-15 12:53:53 +0530

    parallelBfs.cpp

--- parallelBfs.cpp
+++ parallelBfs.cpp
@@ -51,3 +51,27 @@ int main() {
 
     return 0;
 }
+
+/*Explanation : 
+
+First, we have a function called parallelBFS that takes the graph, a list of visited nodes, and the starting node as input. This function performs the breadth-first search algorithm in parallel using OpenMP.
+
+Inside the function, we have a queue that stores the nodes to be processed. We start by adding the starting node to the queue and marking it as visited.
+
+Next, we enter a loop that continues until the queue is empty. This loop represents each level of the BFS traversal.
+
+Within this loop, we divide the work among multiple threads using OpenMP. Each thread processes a subset of nodes from the current level simultaneously.
+
+We use the #pragma omp parallel for directive to indicate that the following loop should be executed in parallel. It automatically distributes the work among the available threads.
+
+Inside the loop, we retrieve a node from the queue in a thread-safe manner. We use the #pragma omp critical directive to ensure that only one thread accesses the queue at a time. This prevents conflicts and data corruption.
+
+Once a node is retrieved, we process it by printing a message indicating that the node has been visited.
+
+Then, we explore the neighbors of the current node. If a neighbor is connected to the current node and has not been visited before, we mark it as visited and add it to the queue for further exploration. Again, we use the #pragma omp critical directive to ensure that the queue is accessed safely by only one thread at a time.
+
+The process continues until all nodes have been visited.
+
+Finally, in the main function, we create a graph represented as an adjacency matrix and a list of visited nodes. We call the parallelBFS function, passing in the graph, visited list, and the starting node index.
+
+When the program runs, it performs parallel breadth-first search on the graph, printing the visited nodes to the console. */
